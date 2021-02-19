0dce3291a0c2474d7ac36ea8ab2ec5be868bb343
Duc Le <duc.le@stfc.ac.uk>
2021-02-19 12:24:01 +0000

    Add euphonic version requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,14 @@ try:
 except ImportError:
     from distutils.core import setup
 
-euphonic_ver = '0.4.0'
+def get_euphonic_version():
+    # gets the required euphonic version from `min_requirements.txt` file
+    import os
+    curdir = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(curdir, 'min_requirements.txt')) as minreq:
+        verstr = [req for req in minreq if 'euphonic' in req]
+    return verstr[0].split('=')[1].strip()
+
 
 setup(name='euphonic_horace',
       version=versioneer.get_version(),
@@ -14,5 +21,5 @@ setup(name='euphonic_horace',
       author_email='rebecca.fair@stfc.ac.uk',
       url='https://github.com/pace-neutrons/euphonic_horace',
       packages=['euphonic_horace'],
-      install_requires=['euphonic>=' + euphonic_ver],
+      install_requires=['euphonic>=' + get_euphonic_version()],
      )
