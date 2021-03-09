ac8c5b127d4a8ec73e5e4b297a65645dac829fed
Duc Le <duc.le@stfc.ac.uk>
2021-03-09 12:45:29 +0000

    Fix [phonopy_reader] requirement; CI now fails if test fails

--- apply_requirements.py
+++ apply_requirements.py
@@ -3,20 +3,24 @@ import subprocess
 
 pipcmd = [sys.executable, '-m', 'pip', 'install']
 
-def clone_euphonic():
+def clone_euphonic(suffix=''):
     subprocess.check_call(['git', 'clone', 'https://github.com/pace-neutrons/euphonic'])
-    subprocess.check_call(pipcmd + ['.'], cwd='euphonic')
+    subprocess.check_call(pipcmd + ['.' + suffix], cwd='euphonic')
 
 
 def install_requirements(requirements_file='min_requirements.txt'):
     with open(requirements_file, 'r') as req:
         packages = req.read().strip().split('\n')
-    euphonic = [pp for pp in packages if 'euphonic' in pp]
-    packages.pop(packages.index(euphonic[0]))
+    euphonic = [pp for pp in packages if 'euphonic' in pp][0]
+    packages.pop(packages.index(euphonic))
+    if '[' in euphonic and ']' in euphonic:
+        suffix = f"[{euphonic.split('[')[1].split(']')[0]}]"
+    else:
+        suffix = ''
     try:
-        subprocess.check_call(pipcmd + euphonic)
+        subprocess.check_call(pipcmd + [euphonic])
     except subprocess.CalledProcessError:
-        clone_euphonic()
+        clone_euphonic(suffix)
     subprocess.check_call(pipcmd + packages)
 
 
