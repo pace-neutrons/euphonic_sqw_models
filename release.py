fca46c9ff49818962d10df256880b4a8ab8b9179
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2021-04-20 10:33:02 +0100

    Update release.py
    - Remove PyPI references (euphonic_sqw_models will not release on PyPI)
    - Add checks for Euphonic version requirements in min_requirements.txt
    - Fix CHANGELOG.rst parsing

--- release.py
+++ release.py
@@ -4,6 +4,10 @@ import os
 import re
 import requests
 import subprocess
+import warnings
+
+from packaging import version
+
 from euphonic_sqw_models import __version__
 
 
@@ -16,22 +20,52 @@ def main():
     if args.github:
         release_github(test)
 
-    if args.pypi:
-        release_pypi(test)
+def check_euphonic_version():
+    """
+    Check euphonic_sqw_models has a release version of Euphonic as a
+    requirement, and raise an Exception if not. If so, check that this
+    version is Euphonic's latest release version and raise a warning if
+    not
+    """
+    req_file = 'min_requirements.txt'
+    with open(req_file) as f:
+        lines = f.readlines()
+    found_euphonic_requirement = False
+    for line in lines:
+        if 'euphonic' in line:
+            found_euphonic_requirement = True
+            if not '=' in line:
+                raise Exception(f'Release versions of euphonic_sqw_models '
+                                f'should be compatible with a Euphonic '
+                                f'release version! Expected {line.strip()} to '
+                                f'contain a "="')
+            euphonic_ver = line.strip()[-5:]
+            response = requests.get(
+                    'https://pypi.org/pypi/euphonic/json')
+            euphonic_rel_vers = [version.parse(x)
+                                 for x in list(response.json()['releases'].keys())]
+            latest_euphonic_release = max(euphonic_rel_vers)
+            if str(latest_euphonic_release) != euphonic_ver:
+                warnings.warn(f'The Euphonic version specified in {req_file} '
+                              f'({euphonic_ver}) is not the latest release '
+                              f'version ({latest_euphonic_release}). Are you '
+                              f'sure this is correct?')
+    if not found_euphonic_requirement:
+        raise Exception(f'Euphonic requirement not found in {req_file}')
 
 
 def release_github(test=True):
+    check_euphonic_version()
     with open('CHANGELOG.rst') as f:
         changelog = f.read()
-    hor_eu_interface_ver = 'v' + __version__
-    changelog_ver = re.findall('`?(v\d+\.\d+\.\S+)\s', changelog)[0]
-    if hor_eu_interface_ver != changelog_ver:
-        #raise Exception((
-        print((
+    eu_sqw_models_ver = 'v' + __version__
+    changelog_ver = re.findall('\n`(v\d+\.\d+\.\S+)\s', changelog)[0]
+    if eu_sqw_models_ver != changelog_ver:
+        raise Exception((
             f'VERSION and CHANGELOG.rst version mismatch!\n'
-            f'VERSION: {hor_eu_interface_ver}\nCHANGELOG.rst: '
+            f'VERSION: {eu_sqw_models_ver}\nCHANGELOG.rst: '
             f'{changelog_ver}'))
-    desc = re.search('`v\d+\.\d+\.\S+.*?^-+\n(.*)', changelog,
+    desc = re.search('`v\d+\.\d+\.\S+.*?^-+\n(.*?)^`v', changelog,
                      re.DOTALL | re.MULTILINE).groups()[0].strip()
 
     payload = {
@@ -51,25 +85,12 @@ def release_github(test=True):
             headers={"Authorization": "token " + os.environ["GITHUB_TOKEN"]})
         print(response.text)
 
-
-def release_pypi(test=True):
-    subprocess.run(['rm','-r','dist'])
-    subprocess.run(['rm','-r','build'])
-    subprocess.run(['python', 'setup.py', 'sdist'])
-    if not test:
-        subprocess.run(['twine', 'upload', 'dist/*'])
-
-
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--github',
         action='store_true',
         help='Release on Github')
-    parser.add_argument(
-        '--pypi',
-        action='store_true',
-        help='Release on PyPI')
     parser.add_argument(
         '--notest',
         action='store_true',
