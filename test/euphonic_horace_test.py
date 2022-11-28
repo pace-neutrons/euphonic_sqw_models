505c3e73f51f4d18bee50a1dfa1f8b9321abbdf1
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2022-11-28 16:34:26 +0000

    Improve tests - put in class and mock available memory

--- test/euphonic_horace_test.py
+++ test/euphonic_horace_test.py
@@ -1,4 +1,5 @@
 import os
+from types import SimpleNamespace
 
 import pytest
 import numpy as np
@@ -366,33 +367,50 @@ def test_invalid_kwargs_raises_value_error(kwarg):
     with pytest.raises(ValueError):
         euphonic_sqw_models.CoherentCrystal(fc, **kwarg)
 
-@pytest.mark.psutil
-def test_optimum_chunk():
-    quartz_fc = euphonic.ForceConstants.from_castep(quartz[2]['filename'])
-    quartz_coh = euphonic_sqw_models.CoherentCrystal(quartz_fc)
+class TestChunk:
 
-    nacl_fc = euphonic.ForceConstants.from_json_file(nacl_json[2]['filename'])
-    nacl_coh = euphonic_sqw_models.CoherentCrystal(nacl_fc)
-
-    assert quartz_coh.chunk is not None
-    assert nacl_coh.chunk is not None
-    # Check fewer atoms = larger chunk size
-    assert nacl_coh.chunk > quartz_coh.chunk
-
-@pytest.fixture
-def mocked_psutil(mocker):
-    # Mock import of psutil to raise ModuleNotFound error
-    import builtins
-    real_import = builtins.__import__
-
-    def mocked_import(name, *args, **kwargs):
-        if name == 'psutil':
-            raise ModuleNotFoundError
-        return real_import(name, *args, **kwargs)
-    mocker.patch('builtins.__import__', side_effect=mocked_import)
-
-def test_optimum_chunk_without_psutil_warns_sets_chunk_to_5000(mocked_psutil):
-    quartz_fc = euphonic.ForceConstants.from_castep(quartz[2]['filename'])
-    with pytest.warns(UserWarning):
+    @pytest.mark.psutil
+    def test_optimum_chunk_larger_with_smaller_cell(self):
+        quartz_fc = euphonic.ForceConstants.from_castep(quartz[2]['filename'])
         quartz_coh = euphonic_sqw_models.CoherentCrystal(quartz_fc)
-    assert quartz_coh.chunk == 5000
+
+        nacl_fc = euphonic.ForceConstants.from_json_file(nacl_json[2]['filename'])
+        nacl_coh = euphonic_sqw_models.CoherentCrystal(nacl_fc)
+
+        assert quartz_coh.chunk is not None
+        assert nacl_coh.chunk is not None
+        # Check fewer atoms = larger chunk size
+        assert nacl_coh.chunk > quartz_coh.chunk
+
+    @pytest.mark.psutil
+    def test_optimum_chunk_mocked_psutil_virtualmemory(self, mocker):
+        available_memory = 1e5
+        psutil_virtual_memory = SimpleNamespace()
+        psutil_virtual_memory.available = available_memory
+        mocker.patch('psutil.virtual_memory',
+                     return_value=psutil_virtual_memory)
+
+        quartz_fc = euphonic.ForceConstants.from_castep(quartz[2]['filename'])
+        quartz_coh = euphonic_sqw_models.CoherentCrystal(quartz_fc)
+        n_atoms = 9
+        evec_bytes_per_qpt = 16*(3*n_atoms)**2
+        assert quartz_coh.chunk == int(available_memory/(10*evec_bytes_per_qpt))
+
+    @pytest.fixture
+    def mocked_psutil_notfound(self, mocker):
+        # Mock import of psutil to raise ModuleNotFound error
+        import builtins
+        real_import = builtins.__import__
+
+        def mocked_import(name, *args, **kwargs):
+            if name == 'psutil':
+                raise ModuleNotFoundError
+            return real_import(name, *args, **kwargs)
+        mocker.patch('builtins.__import__', side_effect=mocked_import)
+
+    def test_optimum_chunk_without_psutil_warns_sets_chunk_to_5000(
+            self, mocked_psutil_notfound):
+        quartz_fc = euphonic.ForceConstants.from_castep(quartz[2]['filename'])
+        with pytest.warns(UserWarning):
+            quartz_coh = euphonic_sqw_models.CoherentCrystal(quartz_fc)
+        assert quartz_coh.chunk == 5000
